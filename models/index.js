0f330329f781f64f90d40427f33968ca787cea82
Wumbo-dot <94250514+Wumbo-dot@users.noreply.github.com>
2022-07-21 21:39:33 -0500

    updated hash password

--- models/index.js
+++ models/index.js
@@ -1,3 +1,8 @@
 const User = require('./User');
+const bcrypt = requrie('bcrypt');
+
+bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash) {
+    //Store hash in your password db.
+});
 
 module.exports = { User };
